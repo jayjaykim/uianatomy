240b86ca25197b4301db9932d772988f1e7db3dc
jayjaykim <jayjaylab.ceo@gmail.com>
2016-07-17 20:44:18 +0900

    add GET request to https://www.instagram.com/ioi_fanpage

--- app/src/main/java/com/jayjaylab/ui/anatomy/MainActivity.java
+++ app/src/main/java/com/jayjaylab/ui/anatomy/MainActivity.java
@@ -8,14 +8,52 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.jayjaylab.ui.anatomy.model.data.instagram.Entries;
+import com.jayjaylab.ui.anatomy.model.logic.InstagramLoader;
+import com.jayjaylab.ui.anatomy.util.Log;
+import org.jsoup.nodes.Document;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
+        setViews();
+
+        Observable<Entries> observable = InstagramLoader.getData();
+        observable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Entries>() {
+            @Override
+            public void onCompleted() {
+                if(Log.DEBUG) Log.d(null);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                if(Log.DEBUG) e.printStackTrace();
+            }
+
+            @Override
+            public void onNext(Entries entries) {
+                if(Log.DEBUG) Log.d("entries : " + entries);
+            }
+        });
+    }
+
+    void setViews() {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
